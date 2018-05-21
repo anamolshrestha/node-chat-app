330b6a235fd0e0e3ab53afa32307566304ae8e01
Anamol Shrestha <anamolshrestha@outlook.com>
2018-05-21 00:37:56 -0500

    Great new user and alert other

--- server/server.js
+++ server/server.js
@@ -15,12 +15,30 @@ io.on('connection',(socket)=>{
    
    socket.on('createMessage',(message)=>{
     console.log(message);
-    io.emit('newMessage',{
-        from:message.from,
-        text:message.text,
-        createdAt: new Date().getTime()
-    });
+
+    // io.emit('newMessage',{
+    //     from:message.from,
+    //     text:message.text,
+    //     createdAt: new Date().getTime()
+    // });
+    // socket.broadcast.emit('newMessage',{
+    //     from:message.from,
+    //     text:message.text,
+    //     createdAt: new Date().getTime()
+    // });
    }); 
+
+   socket.emit('newMessage',{
+    from:'Admin',
+    text:"Welcome to the chat app",
+    createdAt: new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage',{
+    from:"Admin",
+    text:"New user joined",
+    createdAt: new Date().getTime()
+});
 
     socket.on('disconnect',()=>{
         console.log('Disconncted with the client');
