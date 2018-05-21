f917224dbd304569c754fa5a8992af20bb77c7ad
Anamol Shrestha <anamolshrestha@outlook.com>
2018-05-20 23:29:47 -0500

    Socket.io added

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname,'../public');
 const port = process.env.PORT || 3000
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+
+    socket.on('disconnect',()=>{
+        console.log('Disconncted with the client');
+    });
+});
+
+
+server.listen(port,()=>{
     console.log('The server is up on port 3000');
 });
 
